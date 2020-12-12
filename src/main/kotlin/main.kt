a26f7bc7b294a476da79c067d94c4a6c70789006
TaehyungKim_DeskTop <choir0118@gmail.com>
2020-12-13 02:03:43 +0900

    list, set, map 기본 사용 테스트.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,79 @@
+
+fun List<String>.getShortWordsTo(shortWords: MutableList<String>, maxLength: Int) {
+    this.filterTo(shortWords) { it.length <= maxLength }
+
+    val articles = setOf("a", "A", "an", "An", "the", "The")
+    shortWords -= articles
+}
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    mutableTest()
+    println("---------")
+    setTest()
+    println("---------")
+    setTest2()
+    println("---------")
+    mapTest()
+    println("---------")
+    mapTest2()
+    println("---------")
+    mapTest3()
+}
+
+fun mapTest() {
+    val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 1)
+
+    println("All keys: ${numbersMap.keys}")
+    println("All values: ${numbersMap.values}")
+    if ("key2" in numbersMap) println("Value by key \"key2\": ${numbersMap["key2"]}")
+    if (1 in numbersMap.values) println("The value 1 is in the map")
+    if (numbersMap.containsValue(1)) println("The value 1 is in the map") // same as previous
+}
+
+fun mapTest2() {
+    val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 1)
+    val anotherMap = mapOf("key2" to 2, "key1" to 1, "key4" to 1, "key3" to 3)
+
+    println("The maps are equal: ${numbersMap == anotherMap}")
+}
+
+fun mapTest3() {
+    val numbersMap = mutableMapOf("one" to 1, "two" to 2)
+    numbersMap.put("three", 3)
+    numbersMap["one"] = 11
+
+    println(numbersMap)
+}
+
+fun setTest() {
+    val numbers = setOf(1, 2, 3, 4)
+    println("Number of elements: ${numbers.size}")
+    if (numbers.contains(1)) println("1 is in the set")
+
+    val numbersBackwards = setOf(4, 3, 2, 1)
+    println("The sets are equal: ${numbers == numbersBackwards}")
+}
+
+fun setTest2() {
+    val numbers = setOf(1, 2, 3, 4)  // LinkedHashSet is the default implementation
+    val numbersBackwards = setOf(4, 3, 2, 1)
+
+    println(numbers.first() == numbersBackwards.first())
+    println(numbers.first() == numbersBackwards.last())
+}
+
+fun mutableTest() {
+    val numbers = mutableListOf(1, 2, 3, 4)
+    numbers.add(5)
+    numbers.removeAt(1)
+    numbers[0] = 0
+    numbers.shuffle()
+    println(numbers)
+}
+
+fun listTest() {
+    val words = "A long time ago in a galaxy far far away".split(" ")
+    val shortWords = mutableListOf<String>()
+    words.getShortWordsTo(shortWords, 3)
+    println(shortWords)
 }
